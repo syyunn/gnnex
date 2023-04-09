aacd661ad41a4b3558faf07ddfb6b98d1100b584
Suyeol Yun <syyun@mit.edu>
2023-04-09 10:38:55 -0400

    comment out rs

--- hetero/main.py
+++ hetero/main.py
@@ -7,15 +7,15 @@ import random
 
 import torch
 
-# Set the random seed for reproducibility
-seed = 17806
-random.seed(seed)
-np.random.seed(seed)
-torch.manual_seed(seed)
-if torch.cuda.is_available():
-    torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+# # Set the random seed for reproducibility
+# seed = 17806
+# random.seed(seed)
+# np.random.seed(seed)
+# torch.manual_seed(seed)
+# if torch.cuda.is_available():
+#     torch.cuda.manual_seed(seed)
+#     torch.backends.cudnn.deterministic = True
+#     torch.backends.cudnn.benchmark = False
 
 # Open the file in binary read mode and unpickle the data
 with open('/home/gridsan/syun/gnnex/data/hetero_graph_data.pkl', "rb") as f:
