1aa1c4ee1ee21cdf43761257550836d366cd23b6
Suyeol Yun <syyun@mit.edu>
2023-04-20 19:23:33 -0400

    add all

--- hetero/visualize-barplots-subplots.py
+++ hetero/visualize-barplots-subplots.py
@@ -8,31 +8,33 @@ df = pd.read_csv('log_results.csv')
 # Find the highest values among epochs in each group
 df_max_epoch = df.groupby(['edge_type_removed', 'train_test', 'fold']).max('epoch').reset_index()
 
-# Split the data into two groups based on edge_type_removed
-df_buy_sell = df_max_epoch[df_max_epoch['edge_type_removed'].str.contains('buy-sell &')]
-df_other = df_max_epoch[~df_max_epoch['edge_type_removed'].str.contains('buy-sell &')]
-
 # Set the figure size (width, height) in inches
-plt.figure(figsize=(15, 6))  # Adjust the width value as needed
+plt.figure(figsize=(15, 9))  # Adjust the width and height values as needed
 
-# Create the first subplot
-plt.subplot(1, 2, 1)
-sns.pointplot(data=df_buy_sell, x='train_test', y='auc_roc', hue='fold', ci='sd', capsize=0.1, dodge=True, linestyles='')
-plt.xlabel('Train/Test')
+# Create three separate subplots
+plt.subplot(3, 1, 1)
+sns.pointplot(data=df_max_epoch[df_max_epoch['edge_type_removed'].str.count('&') == 0], x='edge_type_removed', y='auc_roc', hue='train_test', ci='sd', capsize=0.1, dodge=True, linestyles='')
+plt.xlabel('Edge Type Removed')
 plt.ylabel('AUC-ROC')
-plt.title('AUC-ROC for "buy-sell &" Edge Type Removed')
-plt.legend(title='Fold')
+plt.title('AUC-ROC for No "&" in Edge Type Removed')
+plt.legend(title='Train/Test')
 
-# Create the second subplot
-plt.subplot(1, 2, 2)
-sns.pointplot(data=df_other, x='train_test', y='auc_roc', hue='edge_type_removed', ci='sd', capsize=0.1, dodge=True, linestyles='')
-plt.xlabel('Train/Test')
+plt.subplot(3, 1, 2)
+sns.pointplot(data=df_max_epoch[df_max_epoch['edge_type_removed'].str.count('&') == 1], x='edge_type_removed', y='auc_roc', hue='train_test', ci='sd', capsize=0.1, dodge=True, linestyles='')
+plt.xlabel('Edge Type Removed')
 plt.ylabel('AUC-ROC')
-plt.title('AUC-ROC for Other Edge Types Removed')
-plt.legend(title='Edge Type Removed', loc='center left', bbox_to_anchor=(1, 0.5))
+plt.title('AUC-ROC for One "&" in Edge Type Removed')
+plt.legend(title='Train/Test', loc='center left', bbox_to_anchor=(1, 0.5))
+
+plt.subplot(3, 1, 3)
+sns.pointplot(data=df_max_epoch[df_max_epoch['edge_type_removed'].str.count('&') == 2], x='edge_type_removed', y='auc_roc', hue='train_test', ci='sd', capsize=0.1, dodge=True, linestyles='')
+plt.xlabel('Edge Type Removed')
+plt.ylabel('AUC-ROC')
+plt.title('AUC-ROC for Two "&"s in Edge Type Removed')
+plt.legend(title='Train/Test', loc='center left', bbox_to_anchor=(1, 0.5))
 
 # Adjust the layout to prevent overlapping titles
-plt.subplots_adjust(wspace=0.5)
+plt.subplots_adjust(hspace=0.5)
 
 # Show the plot
 plt.show()
--- hetero/visualize-barplots.py
+++ hetero/visualize-barplots.py
@@ -8,6 +8,10 @@ df = pd.read_csv('log_results.csv')
 # Find the highest values among epochs in each group
 df_max_epoch = df.groupby(['edge_type_removed', 'train_test', 'fold']).max('epoch').reset_index()
 
+
+# Set the figure size (width, height) in inches
+plt.figure(figsize=(15, 6))  # Adjust the width value as needed
+
 # Plot the point plot with error bars
 sns.pointplot(data=df_max_epoch, x='edge_type_removed', y='auc_roc', hue='train_test', ci='sd', capsize=0.1, dodge=True, linestyles='')
 plt.xlabel('Edge Type Removed')
