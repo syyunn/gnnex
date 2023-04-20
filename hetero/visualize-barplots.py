64627db606016f9acc5850be8391dc6e6ba500c8
Suyeol Yun <syyun@mit.edu>
2023-04-20 17:47:12 -0400

    add all

--- hetero/visualize-barplots.py
+++ hetero/visualize-barplots.py
@@ -9,7 +9,7 @@ df = pd.read_csv('log_results.csv')
 df_max_epoch = df.groupby(['edge_type_removed', 'train_test', 'fold']).max('epoch').reset_index()
 
 # Plot the point plot with error bars
-sns.pointplot(data=df_max_epoch, x='edge_type_removed', y='auc_roc', hue='train_test', ci='sd', capsize=0.1, dodge=True)
+sns.pointplot(data=df_max_epoch, x='edge_type_removed', y='auc_roc', hue='train_test', ci='sd', capsize=0.1, dodge=True, linestyles='')
 plt.xlabel('Edge Type Removed')
 plt.ylabel('AUC-ROC')
 plt.title('AUC-ROC by Edge Type Removed with Error Bars')
